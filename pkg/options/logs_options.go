bb090d9f89241dc1b75521989fdb70c003c26698
孔令飞 <colin404@foxmail.com>
2025-10-01 15:47:56 +0800

    refactor: update LogsOptions

--- pkg/options/logs_options.go
+++ pkg/options/logs_options.go
@@ -9,15 +9,14 @@ package options
 import (
 	"time"
 
-	"github.com/jinzhu/copier"
 	"github.com/spf13/pflag"
 	logsapi "k8s.io/component-base/logs/api/v1"
 )
 
-var _ IOptions = (*LogsOptions)(nil)
+var _ IOptions = (*LogOptions)(nil)
 
-// LogsOptions contains configuration items related to log.
-type LogsOptions struct {
+// LogOptions contains configuration items related to log.
+type LogOptions struct {
 	// Format Flag specifies the structure of log messages.
 	// default value of format is `text`
 	Format string `json:"format,omitempty" mapstructure:"format"`
@@ -30,43 +29,42 @@ type LogsOptions struct {
 	// messages. Higher values enable additional messages. Error messages
 	// are always logged.
 	Verbosity logsapi.VerbosityLevel `json:"verbosity" mapstructure:"verbosity"`
-	// VModule overrides the verbosity threshold for individual files.
-	// Only supported for "text" log format.
-	VModule logsapi.VModuleConfiguration `json:"vmodule,omitempty" mapstructure:"vmodule"`
-	// [Alpha] Options holds additional parameters that are specific
-	// to the different logging formats. Only the options for the selected
-	// format get used, but all of them get validated.
-	// Only available when the LoggingAlphaOptions feature gate is enabled.
-	Options logsapi.FormatOptions `json:"-,omitempty" mapstructure:"-"`
 }
 
-// NewLogsOptions creates an Options object with default parameters.
-func NewLogsOptions() *LogsOptions {
+// NewLogOptions creates an Options object with default parameters.
+func NewLogOptions() *LogOptions {
 	c := logsapi.LoggingConfiguration{}
 	logsapi.SetRecommendedLoggingConfiguration(&c)
 
-	var opts LogsOptions
-	_ = copier.Copy(&opts, &c)
-	return &opts
+	return &LogOptions{
+		Format:         c.Format,
+		FlushFrequency: c.FlushFrequency.Duration.Duration,
+		Verbosity:      c.Verbosity,
+	}
 }
 
-// Validate verifies flags passed to LogsOptions.
-func (o *LogsOptions) Validate() []error {
+// Validate verifies flags passed to LogOptions.
+func (o *LogOptions) Validate() []error {
 	errs := []error{}
 
 	return errs
 }
 
 // AddFlags adds command line flags for the configuration.
-func (o *LogsOptions) AddFlags(fs *pflag.FlagSet, prefixes ...string) {
+func (o *LogOptions) AddFlags(fs *pflag.FlagSet, prefixes ...string) {
 	fs.StringVar(&o.Format, "logs.format", o.Format, "Sets the log format. Permitted formats: json, text.")
 	fs.DurationVar(&o.FlushFrequency, "log.flush-frequency", o.FlushFrequency, "Maximum number of seconds between log flushes.")
 	fs.VarP(logsapi.VerbosityLevelPflag(&o.Verbosity), "logs.verbosity", "", " Number for the log level verbosity.")
-	fs.Var(logsapi.VModuleConfigurationPflag(&o.VModule), "logs.vmodule", "Comma-separated list of pattern=N settings for file-filtered logging (only works for text log format).")
 }
 
-func (o *LogsOptions) Native() *logsapi.LoggingConfiguration {
-	var cfg logsapi.LoggingConfiguration
-	_ = copier.Copy(&cfg, &o)
-	return &cfg
+func (o *LogOptions) Native() *logsapi.LoggingConfiguration {
+	c := logsapi.LoggingConfiguration{}
+	logsapi.SetRecommendedLoggingConfiguration(&c)
+	c.Format = o.Format
+	if o.FlushFrequency != 0 {
+		c.FlushFrequency.Duration.Duration = o.FlushFrequency
+		c.FlushFrequency.SerializeAsString = true
+	}
+	c.Verbosity = o.Verbosity
+	return c
 }
